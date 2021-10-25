6842a82169557100a4f41c7bfbbfb4008c5891ca
kutman <kuu7life@gmail.com>
2021-10-25 20:01:38 +0800

    MVC design pattern implemented

--- Destiny/Controller/ViewController.swift
+++ Destiny/Controller/ViewController.swift
@@ -13,14 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-
     
     @IBAction func choicePressed(_ sender: UIButton) {
+        if let currentTitle  = sender.currentTitle{
+            storyBrain.nextStory(userChoice: currentTitle)
+            updateUI()
+        }
     }
     
+    func updateUI(){
+        storyLabelText.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
 }
 
---
+++ Destiny/Model/Story.swift
@@ -0,0 +1,19 @@
+//
+//  Story.swift
+//  Destiny
+//
+//  Created by Amangeldiev Kutman on 25/10/21.
+//
+
+import Foundation
+
+
+struct Story{
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+    
+
+}
