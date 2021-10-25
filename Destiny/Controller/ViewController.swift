1ffaa505e04a2c123fc557e8358ba528168d7237
kutman <kuu7life@gmail.com>
2021-10-25 19:02:18 +0800

    AutoLyaout Desing & MVC pattern initial commit

--- Destiny/Controller/ViewController.swift
+++ Destiny/Controller/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLabelText: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func choicePressed(_ sender: UIButton) {
+    }
+    
 }
 
